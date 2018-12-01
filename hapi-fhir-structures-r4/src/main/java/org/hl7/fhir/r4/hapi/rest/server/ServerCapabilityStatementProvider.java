0ca88d1b8e2617db067e509cb9b0a18c9c793603
hsolbrig <solbrig@jhu.edu>
2018-12-01 18:50:36 +0100

    Fixed type error in setFhirVersion

--- hapi-fhir-structures-r4/src/main/java/org/hl7/fhir/r4/hapi/rest/server/ServerCapabilityStatementProvider.java
+++ hapi-fhir-structures-r4/src/main/java/org/hl7/fhir/r4/hapi/rest/server/ServerCapabilityStatementProvider.java
@@ -207,7 +207,7 @@ public class ServerCapabilityStatementProvider implements IServerConformanceProv
 
     retVal.setPublisher(myPublisher);
     retVal.setDateElement(conformanceDate());
-    retVal.setFhirVersion(FhirVersionEnum.R4.getFhirVersionString());
+    retVal.setFhirVersion(Enumerations.FHIRVersion.fromCode(org.hl7.fhir.r4.model.Constants.VERSION));
 
     ServletContext servletContext = (ServletContext) (theRequest == null ? null : theRequest.getAttribute(RestfulServer.SERVLET_CONTEXT_ATTRIBUTE));
     String serverBase = getServerConfiguration().getServerAddressStrategy().determineServerBase(servletContext, theRequest);
